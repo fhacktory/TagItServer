20d5cd79a9a0f6451f19dac4a90b705e4d976d21
Martin Delille <martin@phonations.com>
2014-10-12 00:34:27 +0200

    add list tags by url trick

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -61,6 +61,11 @@ class TagsController < ApplicationController
     end
   end
 
+  def get_by_url
+    tags = Tag.where url:params[:url]
+    render json: tags
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tag
