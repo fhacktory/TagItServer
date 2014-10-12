82aca80165f8db92190e81f7e13d168123d138d2
Martin Delille <martin@phonations.com>
2014-10-12 06:46:38 +0200

    get => post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tags
 
-  get "/list_tags_by_url/:url" => "tags#get_by_url"
+  post "/list_tags_by_url" => "tags#get_by_url"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
