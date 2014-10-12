2ef5fc32c74613ac68098baa0c764afd58ab2790
Martin Delille <martin@phonations.com>
2014-10-12 03:38:44 +0200

    destroy all

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -55,7 +55,7 @@ class TagsController < ApplicationController
   # DELETE /tags/1
   # DELETE /tags/1.json
   def destroy
-    @tag.destroy
+    Tag.destroy_all()
     respond_to do |format|
       format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
       format.json { head :no_content }
